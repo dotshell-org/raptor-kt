06f498ad2dac31cd93117363cdf4dead5207358e
leo-grnd <leogarnaud84@gmail.com>
2026-02-08 23:41:28 +0100

    Bump version to 1.2.0 in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "eu.dotshell"
             artifactId = "raptor-kt"
-            version = "1.1.1"
+            version = "1.2.0"
 
             afterEvaluate {
                 from(components["release"])
